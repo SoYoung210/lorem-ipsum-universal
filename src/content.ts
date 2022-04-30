4e8541423de2a477387e3247ef9812fa174ed798
SoYoung210 <ethdud1@gmail.com>
2022-04-30 16:46:22 +0900

    fix: content generate length

--- src/content.ts
+++ src/content.ts
@@ -1,3 +1,5 @@
+import { ParagraphLength } from './model';
+
 const BASE_KO =
   '역시 마찬가지로, 단순히 고통이라는 이유 때문에 고통 그 자체를 사랑하거나 추구하거나 소유하려는 자는 없다. 다만 노역과 고통이 아주 큰 즐거움을 선사하는 상황이 때로는 발생하기 때문에 고통을 찾는 사람이 있는 것이다. 간단한 예를 들자면, 모종의 이익을 얻을 수도 없는데 힘든 육체적 노력을 기꺼이 할 사람이 우리들 중에 과연 있겠는가? 하지만 귀찮은 일이 뒤따르지 않는 즐거움을 누리는 것을 선택한 사람, 혹은 아무런 즐거움도 생기지 않는 고통을 회피하는 사람을 누가 탓할 수 있겠는가?';
 
@@ -8,19 +10,41 @@ export const CONTENT = {
   ko: BASE_KO,
   en: BASE_EN,
 };
+
+const TEXT_LENGTH: Record<ParagraphLength, number> = {
+  short: 125,
+  medium: 160,
+  long: 240,
+};
 export function generateRandomText(params: {
   paragraphCount: number;
   baseSource: string;
+  /**
+   * short: 125 characters
+   * medium: 160 characters
+   * long: 240 characters
+   */
+  paragraphLength: ParagraphLength;
 }) {
   let result = '';
-  const { paragraphCount, baseSource } = params;
+  const { paragraphCount, baseSource, paragraphLength } = params;
   const wordArray = baseSource.split(' ');
 
   for (let i = 0; i < paragraphCount; i++) {
+    let paragraph = '';
+
     for (let i = 0; i < wordArray.length; i++) {
-      result += wordArray[Math.floor(Math.random() * wordArray.length)] + ' ';
+      const nextContent =
+        wordArray[Math.floor(Math.random() * wordArray.length)] + ' ';
+
+      if (
+        paragraph.length + nextContent.length <
+        TEXT_LENGTH[paragraphLength]
+      ) {
+        paragraph += nextContent;
+      }
     }
-    result += '\n\n';
+    result += `${paragraph}\n\n`;
   }
 
   return result;
